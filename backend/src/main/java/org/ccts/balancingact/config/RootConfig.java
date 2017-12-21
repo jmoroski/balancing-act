5f6c4fa9409be270d5283f6812621317fd836753
Jeff Moroski <balancing-act@moroski.info>
2017-12-21 11:15:11 -0500

    Add backend project and Maven support.
    The backend project is a simple shell that pulls down the necessary Spring
    Web MVC and Servlet 3 libraries to build and deploy the project as a web
    application.  A simple root context, web context, and webapp initializer
    have been added to the project.  The Tomcat Maven plugin has been added to
    the Maven lifecycle.
    
    Building from parent project verifies that the ui project correctly
    installs node and yarn when necessary, and correctly builds the backend
    project.  Executing mvn tomcat7:run successfully launches the webapp in
    Tomcat and the placeholder HTML file renders at both / and /index.html.

---
+++ backend/src/main/java/org/ccts/balancingact/config/RootConfig.java
@@ -0,0 +1,8 @@
+package org.ccts.balancingact.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+
+}
---
+++ backend/src/main/java/org/ccts/balancingact/config/WebConfig.java
@@ -0,0 +1,24 @@
+package org.ccts.balancingact.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "org.ccts.balancingact.controller" })
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+       registry.addResourceHandler("/**").addResourceLocations("/WEB-INF/static/");
+    }
+}
