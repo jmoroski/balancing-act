b96ab46b90fe454be856e6cbab6949435e337cf3
Jeff Moroski <balancing-act@moroski.info>
2017-12-21 13:30:47 -0500

    Add database support to the web application.
    The webapp now configures a database connection, a connection pool, a
    Hibernate SessionFactory and a Spring transaction manager when it starts.
    The database connection is configured using a properties file which
    currently configures an in-memory H2 database.

---
+++ backend/src/main/java/org/ccts/balancingact/config/PersistenceConfig.java
@@ -0,0 +1,63 @@
+package org.ccts.balancingact.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:connection.properties")
+public class PersistenceConfig {
+    @Autowired
+    private Environment env;
+
+    Properties hibernateProperties() {
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+
+        return props;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        org.apache.tomcat.jdbc.pool.DataSource ds = new org.apache.tomcat.jdbc.pool.DataSource();
+        ds.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+        ds.setUrl(env.getProperty("jdbc.url"));
+        ds.setUsername(env.getProperty("jdbc.username"));
+        ds.setPassword(env.getProperty("jdbc.password"));
+
+        return ds;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("org.ccts.balancingact.model");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(final SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+       return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
--- backend/src/main/java/org/ccts/balancingact/config/RootConfig.java
+++ backend/src/main/java/org/ccts/balancingact/config/RootConfig.java
@@ -1,8 +1,9 @@
 package org.ccts.balancingact.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(PersistenceConfig.class)
 public class RootConfig {
-
 }
