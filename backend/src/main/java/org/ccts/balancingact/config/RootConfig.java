24c540d04f87beb5a5d19886ea255c5fbbedda89
Jeff Moroski <balancing-act@moroski.info>
2018-01-04 11:57:43 -0500

    Enable Freemarker templates and scheduled tasks.

--- backend/src/main/java/org/ccts/balancingact/config/RootConfig.java
+++ backend/src/main/java/org/ccts/balancingact/config/RootConfig.java
@@ -1,9 +1,25 @@
 package org.ccts.balancingact.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 
 @Configuration
 @Import(PersistenceConfig.class)
+@EnableScheduling
+@ComponentScans({
+    @ComponentScan(basePackages = "org.ccts.balancingact.task")
+})
 public class RootConfig {
+    @Bean
+    public FreeMarkerConfigurationFactoryBean getFreemarkerConfiguration() {
+        FreeMarkerConfigurationFactoryBean factoryBean = new FreeMarkerConfigurationFactoryBean();
+        factoryBean.setTemplateLoaderPath("classpath:/reports/");
+
+        return factoryBean;
+    }
 }
