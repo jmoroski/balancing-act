275f036c079424df6ddd8ea547a963a3f695b42f
Jeff Moroski <balancing-act@moroski.info>
2018-01-17 08:34:18 -0500

    Allow better serialization of time objects as JSON.
    Upgraded Jackson libraries and added date stuff to the ObjectMapper.
    
    Think I slipped in some cache control also.

--- backend/src/main/java/org/ccts/balancingact/config/WebConfig.java
+++ backend/src/main/java/org/ccts/balancingact/config/WebConfig.java
@@ -2,12 +2,15 @@ package org.ccts.balancingact.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@Import(JacksonConfig.class)
 @EnableWebMvc
 @ComponentScan(basePackages = { "org.ccts.balancingact.controller" })
 public class WebConfig extends WebMvcConfigurerAdapter {
@@ -18,6 +21,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-       registry.addResourceHandler("/**").addResourceLocations("/WEB-INF/static/");
+       registry.addResourceHandler("/**").addResourceLocations("/WEB-INF/static/").setCacheControl(CacheControl.noStore().mustRevalidate());
     }
 }
