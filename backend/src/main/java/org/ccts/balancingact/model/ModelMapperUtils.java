a8f363a75640d2aceecc2bbb360e6117ddb5df1e
Jeff Moroski <balancing-act@moroski.info>
2018-01-29 08:32:19 -0500

    Fix some polymorphic mapping issues.
    The ModelMapper doesn't have a clean way to override type maps to create
    subclassed objects in a collection (like the previous version of this file
    assumed).  This change takes a slightly less elagant approach of very
    explicitly managing subclass creation in a converter.

--- backend/src/main/java/org/ccts/balancingact/model/ModelMapperUtils.java
+++ backend/src/main/java/org/ccts/balancingact/model/ModelMapperUtils.java
@@ -1,5 +1,6 @@
 package org.ccts.balancingact.model;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,30 +8,42 @@ import org.ccts.balancingact.model.api.BillingRuleItem;
 import org.ccts.balancingact.model.api.CalculatedBillingRuleItem;
 import org.ccts.balancingact.model.api.SimpleBillingRuleItem;
 import org.ccts.balancingact.model.db.ServiceTaskItemEntity;
-import org.modelmapper.Condition;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.spi.MappingContext;
 
 public class ModelMapperUtils {
+    private static final Converter<ServiceTaskItemEntity, BillingRuleItem> BILLING_RULE_ITEM_CONVERTER = new Converter<ServiceTaskItemEntity, BillingRuleItem>() {
+        @Override
+        public BillingRuleItem convert(MappingContext<ServiceTaskItemEntity, BillingRuleItem> context) {
+            final ServiceTaskItemEntity entity = context.getSource();
+            if (entity.isCalculated()) {
+                System.out.println("creating calculated item");
+                final CalculatedBillingRuleItem item = new CalculatedBillingRuleItem();
+                item.setDescription(entity.getDescription());
+                item.setId(entity.getId().toString());
+                item.setQuantity(BigDecimal.valueOf(entity.getQuantity()));
+                item.setRate(BigDecimal.valueOf(entity.getRate()));
+
+                return item;
+            } else {
+                System.out.println("creating simple item");
+                final SimpleBillingRuleItem item = new SimpleBillingRuleItem();
+                item.setDescription(entity.getDescription());
+                item.setId(entity.getId().toString());
+                item.setAmount(entity.getAmount());
+
+                return item;
+            }
+        }
+    };
     public static final ModelMapper getInstance() {
         final ModelMapper instance = new ModelMapper();
         instance.getConfiguration()
             .setFieldMatchingEnabled(true)
             .setFieldAccessLevel(AccessLevel.PRIVATE);
-        instance.createTypeMap(ServiceTaskItemEntity.class, BillingRuleItem.class)
-            .include(ServiceTaskItemEntity.class, CalculatedBillingRuleItem.class).setCondition(new Condition<ServiceTaskItemEntity, CalculatedBillingRuleItem>() {
-                @Override
-                public boolean applies(MappingContext<ServiceTaskItemEntity, CalculatedBillingRuleItem> context) {
-                    return context.getSource().isCalculated();
-                }
-            })
-            .include(ServiceTaskItemEntity.class, SimpleBillingRuleItem.class).setCondition(new Condition<ServiceTaskItemEntity, SimpleBillingRuleItem>() {
-                @Override
-                public boolean applies(MappingContext<ServiceTaskItemEntity, SimpleBillingRuleItem> context) {
-                    return !context.getSource().isCalculated();
-                }
-            });
+        instance.addConverter(BILLING_RULE_ITEM_CONVERTER);
 
         return instance;
     }
