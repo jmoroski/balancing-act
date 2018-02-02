b10713db96032a683f7b6933de53dd8dc37a80e6
Jeff Moroski <balancing-act@moroski.info>
2018-02-02 09:14:06 -0500

    Wire up daily cron to actually generate invoices.
    The intent is for this to run every day, and generate invoices that are
    configured (by service billing rules) to be created on that day.
    
    Some current obvious limitations are that it does no lookback to address
    server downtime, and it assumes that those invoices don't already exist.

--- backend/src/main/java/org/ccts/balancingact/task/GenerateInvoicesTask.java
+++ backend/src/main/java/org/ccts/balancingact/task/GenerateInvoicesTask.java
@@ -1,11 +1,19 @@
 package org.ccts.balancingact.task;
 
+import java.time.LocalDate;
+
+import org.ccts.balancingact.dao.InvoiceDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GenerateInvoicesTask {
+    @Autowired
+    private InvoiceDao invoiceDao;
+
     @Scheduled(cron = "0 0 5 * * *")
     public void generate() {
+        this.invoiceDao.createScheduledInvoices(LocalDate.now(), LocalDate.now());
     }
 }
